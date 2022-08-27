1696e2803ad103e45a04ee57d420637911c41740
yc <devenmm2005@gmail.com>
2022-08-27 23:10:43 +0800

    Revise according to CodeGuru's advice.

--- lab-3.py
+++ lab-3.py
@@ -19,8 +19,8 @@ def kickoff_subprocess(cmd, log_file_name):
         file.write(output)
 
 def upload_output_to_S3(log_file_name):
-    f = open(log_file_name, "rb")
-    s3.upload_fileobj(f, "ycp-example-bucket", log_file_name)
+    with open(log_file_name, "rb") as f:
+        s3.upload_fileobj(f, "ycp-example-bucket", log_file_name)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
