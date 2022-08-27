8669882f1176eba7e5345f097e03cb08bc6ab7bc
yc <devenmm2005@gmail.com>
2022-08-27 16:57:14 +0800

    Revise after CodeGuru's review.

--- lab-3.py
+++ lab-3.py
@@ -1,4 +1,4 @@
-import subprocess
+import shlex
 import sys
 import boto3
 import datetime
@@ -7,16 +7,16 @@ s3 = boto3.client('s3')
 
 def main(argv):
     cmd = argv
-    log_file_name = datetime.datetime.now().strftime("%m_%d_%Y") + "_logfile"
+    log_file_name = datetime.datetime.now(datetime.timezone.utc).strftime("%m_%d_%Y") + "_logfile"
     kickoff_subprocess(cmd, log_file_name)
     upload_output_to_S3(log_file_name)
 
 def kickoff_subprocess(cmd, log_file_name):
-    process = subprocess.call(cmd, shell=True)
-    file = open(log_file_name, "a+")
-    timestamp = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
-    file.write(output)
+    process = shlex.quote(cmd)
+    with open(log_file_name, "a+") as file:
+        timestamp = datetime.datetime.now(datetime.timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
+        output = timestamp + " Command: "+ cmd[0] + " | Return Code: " + str(process) + "\n"
+        file.write(output)
 
 def upload_output_to_S3(log_file_name):
     f = open(log_file_name, "rb")
